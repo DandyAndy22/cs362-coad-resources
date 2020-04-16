1efc68de36a91cee2a79970ca947032497c3aec9
clay-town <justinmccune@icloud.com>
2020-04-16 09:45:06 -0700

    Implement test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
+	it {should belong_to(:organization)}
 end
+
+RSpec.describe User do
+	it {should expect(:email).to "justin@mail.com"}
+end
+
