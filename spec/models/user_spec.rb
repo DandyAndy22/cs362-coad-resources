8bf6b8ac3ab3775f6e16281e9369344f7651a157
clay-town <justinmccune@icloud.com>
2020-04-21 15:39:47 -0700

    Implement test for users relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+	
 	describe "attributes" do
 		it "has an email" do
 			user = User.new 
 			expect(user).to respond_to(:email)
 		end
 	end
+
+	describe "relationships" do
+		it "belongs to an organization" do
+			user = User.new
+			expect(user).to belong_to(:organization)
+		end
+	end
+
 end
 	# existence
 	
