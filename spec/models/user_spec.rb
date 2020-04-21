62e172b5b53552e35a2d29c6b43b04223a940372
clay-town <justinmccune@icloud.com>
2020-04-21 15:34:17 -0700

    implement test for user existence

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-	it {should belong_to(:organization)}
-	it "has an email" do
-		user = User.new 
-		expect(user).to respond_to(:email)
+	describe "attributes" do
+		it "has an email" do
+			user = User.new 
+			expect(user).to respond_to(:email)
+		end
 	end
 end
+	# existence
+	
+	# attributes
+		
+	# relationships
+		#it {should belong_to(:organization)}
+	# validations
+
+	# behavior
+
+
+
+
+
+
+
 
