4500cb232d72d832b3b653e646056d57e3837140
clay-town <justinmccune@icloud.com>
2020-04-21 15:59:06 -0700

    Abrastract user test object

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,27 +2,25 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 	
+	let(:user) {User.new}
+
 	describe "attributes" do
 		it "has an email" do
-			user = User.new 
 			expect(user).to respond_to(:email)
 		end
 	end
 
 	describe "relationships" do
 		it "belongs to an organization" do
-			user = User.new
 			expect(user).to belong_to(:organization)
 		end
 	end
 	
 	describe "validations" do
 		it "validates email" do
-			user = User.new
 			expect(user).to validate_presence_of(:email)
 		end
 		it "validates length of email" do
-			user = User.new
 			expect(user).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
 		end
 	end
