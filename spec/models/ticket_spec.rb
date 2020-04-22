366b256a0860a3eb0049e3911fac2c4d314a7029
Andras Mihaly <andrasm96@gmail.com>
2020-04-22 09:14:54 -0700

    Ticket: Test for existence

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+	describe 'Existence' do
+		it 'exists' do
+			ticket = Ticket.new()
+			expect(ticket)
+	  end
+	end 
+	# Relationships
+	# Behavior
+	# Validations
+	# Attributes
+
 end
