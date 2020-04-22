4cd55385a450f3b3c5eae78c2ee3914d2364ccf4
Andras Mihaly <andrasm96@gmail.com>
2020-04-22 09:59:23 -0700

    Ticket: Test Relationships

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,13 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+	let(:ticket) {Ticket.new()}
+
 	describe 'Existence' do
 		it 'exists' do
-			ticket = Ticket.new()
 			expect(ticket)
 	  end
 	end 
-	# Relationships
+
+	describe 'Relationships' do
+		it 'belongs to a region' do
+			expect(ticket).to belong_to(:region)
+		end
+
+		it 'belongs to a resource category' do
+			expect(ticket).to belong_to(:resource_category)
+		end
+
+		it 'belongs to an organization' do
+			expect(ticket).to belong_to(:organization)
+		end
+	end
+
 	# Behavior
 	# Validations
 	# Attributes
