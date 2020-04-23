bdbb96bab2c9323bb1727544a6f671b569cb1813
clay-town <justinmccune@icloud.com>
2020-04-23 08:57:50 -0700

    use a let instead

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,21 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+	let(:region) {Region.new(name: 'The Shire')}
+
+
 	describe "relationships" do
 		it "Expects to belong to" do
-			expect(Region.new).to have_many(:tickets)
+			expect(region).to have_many(:tickets)
 		end
 	end
 
 	describe "attributes" do
 		it "has a name!!!!" do
-			expect(Region.new).to respond_to(:name)
+			expect(region).to respond_to(:name)
 		end
 	end
 
 	describe "validations" do
 		it "has a specified length" do
-			expect(Region.new).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+			expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
 		end
 	end
 
