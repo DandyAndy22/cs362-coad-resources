e42523b3266d9cb7fd210e6ec5d941ad960423c9
clay-town <justinmccune@icloud.com>
2020-04-23 08:50:56 -0700

    fix test

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+	describe "relationships" do
+		it "Expects to belong to" do
+			expect(Region.new).to have_many(:tickets)
+		end
+	end
+
+	# describe "attributes" do
+	# 	it "has a name!!!!" do
+	# 		expect(Region.new).to respond_to(:name)
+	# 	end
+	# end
+
+	# properties
+	# validations
+	# methods
+
+
 end
