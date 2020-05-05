6b30b1a097eafa28312aa8b252642ba45b10810b
clay-town <justinmccune@icloud.com>
2020-05-05 09:27:26 -0700

    Add test for region public user auth

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
+	context 'As a public user' do
+		describe '/regions' do
+			specify { expect(get(:index)).to redirect_to(new_user_session_path) }
+		end
+		#cannot do anything
+		#route to login
+	end
+	context 'As an organization user' do
+		#cannot do anything
+		#route to dashboard
+	end
+	context 'As an admin' do
+		#can do stuff
+	end
 
 end
