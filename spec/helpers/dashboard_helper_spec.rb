14551267401158fedba2cd602b0c60c983459d4d
clay-town <justinmccune@icloud.com>
2020-05-16 13:06:52 -0700

    Implement test for return of create application dashboard

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -32,4 +32,12 @@ RSpec.describe DashboardHelper, type: :helper do
     expect(dashboard_for(user)).to eq('organization_approved_dashboard')
   end
 
+  it "returns create_application_dashboard" do
+    User.any_instance.stub(:admin?).and_return(false)
+    user = User.new
+    user.stub_chain(:organization, :submitted?).and_return(false)
+    user.stub_chain(:organization, :approved?).and_return(false)
+    expect(dashboard_for(user)).to eq('create_application_dashboard')
+  end
+
 end
