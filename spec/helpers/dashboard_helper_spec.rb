211b2e078723ee3e51ea367e2ad1f951ce05892b
clay-town <justinmccune@icloud.com>
2020-05-16 13:09:19 -0700

    add describe block

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -11,33 +11,36 @@ require 'rails_helper'
 # end
 RSpec.describe DashboardHelper, type: :helper do
 
-  it "returns admin_dashboard" do
-    user = double()
-    user.stub(:admin?).and_return(true)
-    expect(dashboard_for(user)).to eq('admin_dashboard')
-  end  
+  describe 'dashboard_for' do
+    it "returns admin_dashboard" do
+      user = double()
+      user.stub(:admin?).and_return(true)
+      expect(dashboard_for(user)).to eq('admin_dashboard')
+    end  
 
-  it "returns organization_submitted_dashboard" do
-    User.any_instance.stub(:admin?).and_return(false)
-    user = User.new
-    user.stub_chain(:organization, :submitted?).and_return(true)
-    expect(dashboard_for(user)).to eq('organization_submitted_dashboard')
+    it "returns organization_submitted_dashboard" do
+      User.any_instance.stub(:admin?).and_return(false)
+      user = User.new
+      user.stub_chain(:organization, :submitted?).and_return(true)
+      expect(dashboard_for(user)).to eq('organization_submitted_dashboard')
+    end
+
+    it "returns organization_approved_dashboard" do
+      User.any_instance.stub(:admin?).and_return(false)
+      user = User.new
+      user.stub_chain(:organization, :submitted?).and_return(false)
+      user.stub_chain(:organization, :approved?).and_return(true)
+      expect(dashboard_for(user)).to eq('organization_approved_dashboard')
+    end
+
+    it "returns create_application_dashboard" do
+      User.any_instance.stub(:admin?).and_return(false)
+      user = User.new
+      user.stub_chain(:organization, :submitted?).and_return(false)
+      user.stub_chain(:organization, :approved?).and_return(false)
+      expect(dashboard_for(user)).to eq('create_application_dashboard')
+    end
+
   end
-
-  it "returns organization_approved_dashboard" do
-    User.any_instance.stub(:admin?).and_return(false)
-    user = User.new
-    user.stub_chain(:organization, :submitted?).and_return(false)
-    user.stub_chain(:organization, :approved?).and_return(true)
-    expect(dashboard_for(user)).to eq('organization_approved_dashboard')
-  end
-
-  it "returns create_application_dashboard" do
-    User.any_instance.stub(:admin?).and_return(false)
-    user = User.new
-    user.stub_chain(:organization, :submitted?).and_return(false)
-    user.stub_chain(:organization, :approved?).and_return(false)
-    expect(dashboard_for(user)).to eq('create_application_dashboard')
-  end
-
+  
 end
