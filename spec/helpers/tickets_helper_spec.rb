967b02e3ac4843c401d652c7ff20acbff9ea441e
clay-town <justinmccune@icloud.com>
2020-05-16 13:33:21 -0700

    Implement test for format_phone_number

--- spec/helpers/tickets_helper_spec.rb
+++ spec/helpers/tickets_helper_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe TicketsHelper, type: :helper do
+
   describe 'format_phone_number' do
+
     it 'returns a normalized phone number' do
       number = "5034226553"
-      expect(format_phone_number(number)).to eq('hello world')
+      expect(format_phone_number(number)).to eq("+1"+number)
     end
+  
   end
+
 end
