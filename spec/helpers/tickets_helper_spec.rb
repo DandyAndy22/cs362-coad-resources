1eda4a7f43e1e9ebb65f5fa0c6d30319d4f62892
clay-town <justinmccune@icloud.com>
2020-05-16 13:26:12 -0700

    push

--- spec/helpers/tickets_helper_spec.rb
+++ spec/helpers/tickets_helper_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TIcketsHelper. For example:
-#
-# describe TIcketsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TicketsHelper, type: :helper do
-
+  describe 'format_phone_number' do
+    it 'returns a normalized phone number' do
+      number = "5034226553"
+      expect(format_phone_number(number)).to eq('hello world')
+    end
+  end
 end
