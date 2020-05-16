b48008e3214d800dd23e205f64cc1da94f02c4ff
clay-town <justinmccune@icloud.com>
2020-05-16 13:20:44 -0700

    implement test for return of page title and base title concat

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,7 +4,14 @@ RSpec.describe ApplicationHelper, type: :helper do
 
 	describe 'full_title' do
 		it 'returns base title if page title is empty' do
-			expect(full_title).to eq('Disaster Resource Network')
+			base_title = 'Disaster Resource Network'
+			expect(full_title).to eq(base_title)
+		end
+
+		it 'returns page title and base btitle' do
+			page_title = 'Hello World'
+			base_title = 'Disaster Resource Network'
+			expect(full_title(page_title)).to eq("#{page_title} | #{base_title}")
 		end
 	end	
 
