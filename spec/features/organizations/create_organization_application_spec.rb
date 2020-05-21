9dc02020bb434ada73496c86dbb9403d5f85b2cd
Andras Mihaly <andrasm96@gmail.com>
2020-05-21 09:49:38 -0700

    Layout Organization application feature test

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
 
+	describe 'Successful Submission' do
+		it 'visits new orgaization application' do
+			
+			log_in_as(:organization)
+			visit(new_organization_application_path)
+		end
+	end
+
 end
