08d8100aab1afa2fc2de95f0a156819803044bb4
clay-town <justinmccune@icloud.com>
2020-06-01 21:51:27 -0700

    Implement test for public user - organizations controller

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 	context 'As a public user' do
-		#direct to login screen
 		describe 'GET #index' do
 			specify { expect(get(:index)).to redirect_to(new_user_session_path) }
 		end
@@ -27,5 +26,4 @@ RSpec.describe DashboardController, type: :controller do
 		end
 	end
 
-	
 end
--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
 
+	context 'As a public user' do
+		describe 'GET #index' do
+			specify { expect(get(:index)).to redirect_to(new_user_session_path) }
+		end
+	end
+
 end
