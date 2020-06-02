12ed65aaeb15c51e33ed5fff7d6a7012d0011a01
clay-town <justinmccune@icloud.com>
2020-06-01 21:53:10 -0700

    Implement test for organization user - organizations controller

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -16,4 +16,14 @@ RSpec.describe OrganizationsController, type: :controller do
 			specify	{ expect(get(:index)).to be_successful }
 		end	
 	end
+
+	context 'As an organization user' do
+		let(:user) { create(:user) }
+		before(:each) { sign_in(user) }
+
+		describe 'GET #index' do
+			specify { expect(get(:index)).to be_successful }
+		end
+	end
+
 end
