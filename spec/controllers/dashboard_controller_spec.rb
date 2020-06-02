80598e75d86757dd8b37db931e9823ba61cece68
clay-town <justinmccune@icloud.com>
2020-06-01 21:24:47 -0700

    Implement dashboard controller test for public user

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
+	context 'As a public user' do
+		#direct to login screen
+		describe 'GET #index' do
+			specify { expect(get(:index)).to redirect_to(new_user_session_path) }
+		end
+	end
 
+	
 end
