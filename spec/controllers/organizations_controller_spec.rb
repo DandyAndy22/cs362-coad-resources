791a728652225f806b0d175a9c563d2635cab650
clay-town <justinmccune@icloud.com>
2020-06-01 21:52:19 -0700

    Implement test for admin user - organizations controller

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe OrganizationsController, type: :controller do
 		end
 	end
 
+	context 'As an admin' do
+		let(:admin_user) { create(:user, :admin) }
+		before(:each) { sign_in(admin_user) }
+
+		describe 'GET #index' do
+			specify	{ expect(get(:index)).to be_successful }
+		end	
+	end
 end
